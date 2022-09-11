5437f9caf0907b4a0421b7151f9d07e53f1a25b4
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2022-09-11 14:59:16 +0530

    Spring Webservices SOAP

---
+++ SpringWebserviceSOAP/src/main/java/com/nadin/springwebservicesoap/SpringWebserviceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.nadin.springwebservicesoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserviceSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserviceSoapApplication.class, args);
+    }
+
+}
---
+++ SpringWebserviceSOAP/src/test/java/com/nadin/springwebservicesoap/SpringWebserviceSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadin.springwebservicesoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
